727f946330c57428032c1efc6644084b3f78119a
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2025-07-05 23:36:38 +0530

    Add sign-in and sign-up logic

---
+++ src/routes/(auth)/sign-in/+page.server.ts
@@ -0,0 +1,47 @@
+import type { PageServerLoad, Actions } from "./$types";
+import { redirect, fail } from "@sveltejs/kit";
+import { isValidPassword } from "$lib/utils";
+import isEmail from "validator/lib/isEmail";
+
+export const load: PageServerLoad = async ({ locals, url }) => {
+  if (locals.user) {
+    const redirectTo = url.searchParams.get("redirect-to");
+    if (redirectTo) return redirect(307, `/${redirectTo.slice(1)}`);
+    return redirect(307, "/dashboard");
+  }
+
+  return {};
+};
+
+export const actions: Actions = {
+  default: async ({ locals, request, url }) => {
+    if (locals.user) {
+      return fail(405, { message: "Method not allowed." });
+    }
+
+    const formData = await request.formData();
+    const email = formData.get("email")?.toString() || "";
+    const password = formData.get("password")?.toString() || "";
+
+    if (!isEmail(email) || !isValidPassword(password)) {
+      return fail(401, { message: "Invalid credentials." });
+    }
+
+    try {
+      await locals.pb.collection("users").authWithPassword(email, password);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
+      console.log("[! /sign-in] =>", err);
+
+      if (err?.status === 400 && err?.data?.message === "Failed to authenticate.") {
+        return fail(401, { message: "Invalid credentials." });
+      }
+
+      return fail(500, { message: "An unexpected error occurred. Please try again later." });
+    }
+
+    const redirectTo = url.searchParams.get("redirect-to");
+    if (redirectTo) return redirect(307, `/${redirectTo.slice(1)}`);
+    return redirect(307, "/dashboard");
+  },
+};
---
+++ src/routes/(auth)/sign-out/+server.ts
@@ -0,0 +1,15 @@
+import type { RequestHandler } from "./$types";
+import { redirect } from "@sveltejs/kit";
+
+export const DELETE: RequestHandler = async ({ locals }) => {
+  if (!locals.user) {
+    return redirect(307, "/sign-in");
+  }
+
+  locals.pb.authStore.clear();
+  locals.user = null;
+
+  return redirect(307, "/sign-in");
+};
+
+export const POST: RequestHandler = DELETE;
