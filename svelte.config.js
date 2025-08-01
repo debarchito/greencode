8428c9473a68a352a62e1468df35e1ea9a47b7b0
Debarchito Nath <debarchiton@proton.me>
2025-08-01 12:19:52 +0530

    enable async svelte and kit remote functions

--- svelte.config.js
+++ svelte.config.js
@@ -4,10 +4,18 @@ import adapter from "@deno/svelte-adapter";
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
   preprocess: vitePreprocess(),
+  compilerOptions: {
+    experimental: {
+      async: true,
+    },
+  },
   kit: {
     adapter: adapter(),
     csrf: {
       checkOrigin: false,
+    },
+    experimental: {
+      remoteFunctions: true,
     },
   },
 };
